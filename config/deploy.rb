79fb85f822f3211ae07022535fdfa659739b388a
Tristan Stanley <tristan@zhurosoft.com>
2013-10-09 23:48:23 -0700

    Updated deploy again.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,25 +5,39 @@ set :user, "tristan"
 set :ssh_options, { :forward_agent => true }
 set :use_sudo, false
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
 role :web, "162.243.128.152"                          # Your HTTP server, Apache/etc
 role :app, "162.243.128.152"                          # This may be the same as your `Web` server
 role :db,  "162.243.128.152", :primary => true # This is where Rails migrations will run
 role :db,  "162.243.128.152"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+require 'capistrano/ext/multistage'
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+after "deploy:update", "deploy:cleanup" 
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  
+  task :start do
+    
+  end
+  
+  task :stop do
+    
+  end
+
+  desc "Restart the servers"
+  task :restart, :roles => :app do
+    
+    # Reduce the number of workers by 10
+    run "for run in {1..10}; do kill -TTOU `cat #{File.join(shared_path,'pids','unicorn.pid')}`; done"
+
+    # Start new workers
+    run "kill -USR2 `cat #{File.join(shared_path,'pids','unicorn.pid')}`"
+
+    sleep 120
+
+    # Kill off old workers
+    run "kill `cat #{File.join(shared_path,'pids','unicorn.pid.oldbin')}`"
+    
+  end
+
+end
